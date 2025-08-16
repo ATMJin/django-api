f843d65c02fe95cbcf90f8173c7a3736b125dfa4
ATMJin0912 <atmjin0912@outlook.com>
2025-08-16 15:10:05 +0800

    新增 hello_world API 端點以支援 POST 請求，並新增 HelloWorldView 類別以提供類別型視圖

--- server/apps/management/views.py
+++ server/apps/management/views.py
@@ -1,8 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # Create your views here.
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def hello_world(request):
-    return Response({"message": "Hello, world!"})
+    if request.method == "GET":
+        return Response({"message": "Hello, GET world!"})
+    elif request.method == "POST":
+        return Response({"message": "Hello, POST world!"})
+
+
+class HelloWorldView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, world! GET"})
+
+    def post(self, request):
+        return Response({"message": "Hello, world! POST"})
